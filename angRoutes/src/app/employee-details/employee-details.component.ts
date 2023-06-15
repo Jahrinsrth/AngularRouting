e49a923f641a067380fc90cabf75d23a260f10b5
Jahrin Sarooth <38609524+Jahrinsrth@users.noreply.github.com>
2023-06-15 22:13:54 +0530

    route has been updated

--- angRoutes/src/app/employee-details/employee-details.component.ts
+++ angRoutes/src/app/employee-details/employee-details.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute,Router,ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
   styleUrls: ['./employee-details.component.css']
 })
-export class EmployeeDetailsComponent {
+export class EmployeeDetailsComponent implements  OnInit{
+
+  employeeId :any;
+
+  constructor(private route :ActivatedRoute, private router : Router){
+
+  }
+  ngOnInit(): void {
+      //let id  = this.route.snapshot.paramMap.get('id');
+      this.route.paramMap.subscribe( (param:ParamMap) => {
+          let id =  param.get('id');
+          this.employeeId = id 
+      })
+      //this.employeeId = id
+      //console.log(id);
+  }
+
+  OnPrevious(){
+      let id  =  parseInt(this.employeeId) - 1;
+      this.router.navigate(['/employee', id]);
+  }
+
+  OnNext(){
+          let id  = parseInt(this.employeeId) + 1;
+          this.router.navigate(['/employee', id]); 
+  }
 
 }
--- angRoutes/src/app/employee/employee.component.ts
+++ angRoutes/src/app/employee/employee.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, importProvidersFrom } from '@angular/core';
+import { Router,ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent {
+
+
+  constructor(private router :Router, private route:ActivatedRoute){
+
+  }
+
+  employess = [
+    {"Id" : 1 , "Name" : "Steve"},
+    {"Id" : 2 , "Name" : "Henry"},
+    {"Id" : 3 , "Name" : "Mike"},
+    {"Id" : 4, "Name" : "Mary"}
+  ]
+
+  OnSelect(emp:any){
+ //    this.router.navigate(["/employee", emp.Id]);
+
+    this.router.navigate([emp.Id], {relativeTo: this.route});  
+ console.log(emp);
+  }
 
 }
